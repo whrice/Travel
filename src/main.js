6f2b5e8de900dc753c8ac7f068c45d30642fdc7c
wang huan <wanghuanrice@163.com>
2019-07-21 16:05:05 +0800

    change

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
 // 不同设备表现为同一个css文件
 import './assets/styles/border.css'
@@ -10,6 +11,9 @@ import './assets/styles/border.css'
 import fastClick from 'fastclick'
 // 解决移动端300ms点击延迟的问题
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 /* eslint-disable no-new */
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+ <div class='wrapper'>
+  <swiper :options="swiperOption" >
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class='swiper-img' :src="item.imgUrl">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+
+ </div>
+</template>
+<script>
+export default {
+  name: "HomeSwiper",
+  data () {
+    return{
+       swiperOption: {
+          pagination:'.swiper-pagination',
+          loop:true
+      },
+      swiperList:[{
+         id:'0001',
+         imgUrl:'/static/img/slide1.d0cec51.jpg'
+      },{
+         id:'0002',
+         imgUrl:'/static/img/slide2.5a57171.jpg'
+      },{
+          id:'0003',
+           imgUrl:'/static/img/slide3.f1122ad.jpg'
+      }]
+    }
+  }
+};
+</script>
+<style lang='stylus' scoped>
+.wrapper>>>  .swiper-pagination-bullet-active
+        background #fff !important
+.wrapper
+    width 100%
+    height 0
+    overflow  hidden
+    padding-bottom 32.5%   //保持宽高比自适应为32.5%
+    .swiper-img
+        width 100%
+</style>
